156bccb81cc2ecb7d4856b43152f586858eb92f9
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-09 16:09:32 +0200

    Created Travel java class

---
+++ app/src/main/java/com/example/project_assignment/Travel.java
@@ -0,0 +1,9 @@
+package com.example.project_assignment;
+
+public class Travel {
+    private String id;
+    private String name;
+    private String company;
+    private int cost;
+    private String location;
+}
