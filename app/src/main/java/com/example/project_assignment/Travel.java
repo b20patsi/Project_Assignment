b13430df16ba09b204034703cad5cfa9a72d9b97
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-09 16:45:21 +0200

    Updated Travel.java with toString and getters

--- app/src/main/java/com/example/project_assignment/Travel.java
+++ app/src/main/java/com/example/project_assignment/Travel.java
@@ -1,9 +1,32 @@
 package com.example.project_assignment;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Travel {
     private String id;
     private String name;
     private String company;
-    private int cost;
     private String location;
+    private int cost;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCost() {
+        return cost;
+    }
 }
