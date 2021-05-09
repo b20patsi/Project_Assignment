6f1f14f6fb3e2c02532c54042b955014fa767144
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-09 17:20:30 +0200

    Created click listener on about page

--- app/src/main/java/com/example/project_assignment/AboutUs.java
+++ app/src/main/java/com/example/project_assignment/AboutUs.java
@@ -2,13 +2,30 @@ package com.example.project_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUs extends AppCompatActivity {
+
+    private Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        backBtn = findViewById(R.id.moveBack);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("AboutUs ==>", "Back to main page");
+
+                finish();
+            }
+        });
     }
 }
