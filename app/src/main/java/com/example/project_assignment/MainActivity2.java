b78f67c4f0cf2c7b7e94d7c4aeec130e169874e3
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-18 10:32:06 +0200

    Added intent to receive data from main activity

--- app/src/main/java/com/example/project_assignment/MainActivity2.java
+++ app/src/main/java/com/example/project_assignment/MainActivity2.java
@@ -2,14 +2,26 @@ package com.example.project_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        String country = intent.getStringExtra("name_of_country");
+        String location = intent.getStringExtra("name_of_location");
+
+        textView = findViewById(R.id.test_text);
+
+        textView.setText(country + " ligger i " + location);
     }
 }
